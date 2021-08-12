227e6d5f04809c18095ad11662d1b47f0dba933c
formula27 <huangminfa_whu@163.com>
2021-08-13 07:55:07 +0800

    Create partition.cpp

---
+++ partition.cpp
@@ -0,0 +1,34 @@
+//快速排序使用的分区函数
+//记住它的办法，用三个交换作为主线，其中第二次交换，用具体例子画图出来比较容易理解
+//要求能在5分钟内写出来
+int partition(int a[], int start, int end)
+{
+  int index = start;
+  
+  //第一次交换
+  int tmp = a[index];
+  a[index] = a[end];
+  a[end] = tmp;
+  
+  for (int i = 0; i < end; i++)
+  {
+    if (a[i] < a[end])
+    {
+      if (i != index)
+      {
+        //第二次交换
+        tmp = a[index];
+        a[index] = a[i];
+        a[i] = tmp;
+      }
+      index++;
+    }
+  }
+  
+  //第三次交换
+  tmp = a[index];
+  a[index] = a[end];
+  a[end] = tmp;
+  
+  return index;
+}
